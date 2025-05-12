fa05de04e4723a2c291f1968f9cbbc4f51ec01e9
Mochamad luthfi <lthfpratama@gmail.com>
2025-05-12 09:38:22 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+/* toggle */
+const navslide=()=>{
+    const burger= document.querySelector(".burger");
+    const navList= document.querySelector("nav");
+
+    burger.addEventListener("click", ()=> {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navslide();
+
+// clear form before unload
+
+window.onbeforeunload = ()=> {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
